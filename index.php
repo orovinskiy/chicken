c096ddeddc51b7febca4d539f1c662403597c18a
Oleg Rovinskiy <orovinskiy@mail.greenriver.edu>
2020-01-15 14:38:50 -0500

    Load fat free

--- index.php
+++ index.php
@@ -9,3 +9,17 @@
 //error reporting..
 ini_set("display_errors",1);
 error_reporting(E_ALL);
+
+//Require autoload file
+require("vendor/autoload.php");
+
+//Instantiate F3
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function(){
+    echo "hello";
+});
+
+//Run f3
+$f3->run();
