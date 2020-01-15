26b85d76a544c5b97fcdd75bc123f24193e7dcdf
Oleg Rovinskiy <orovinskiy@mail.greenriver.edu>
2020-01-15 14:17:11 -0500

    Added index page with error reporting

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Oleg Rovinskiy
+ * @date 1/15/2020
+ * @url https://github.com/orovinskiy/chicken
+ * This is a chicken webpage
+ */
+//error reporting..
+ini_set("display_errors",1);
+error_reporting(E_ALL);
