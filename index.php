7654f8ddeff98def9b07682cab48e1e7170ee862
Oleg Rovinskiy <orovinskiy@mail.greenriver.edu>
2020-01-15 14:28:53 -0500

    Added and installed composer

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
  * @author Oleg Rovinskiy
  * @date 1/15/2020
  * @url https://github.com/orovinskiy/chicken
- * This is a chicken webpage
+ * This is a chicken web page
  */
+
 //error reporting..
 ini_set("display_errors",1);
 error_reporting(E_ALL);
